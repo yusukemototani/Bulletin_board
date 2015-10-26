f5519864fdfe3272b4e5f533eeabf298d0c7ba9d
yusuke mototani <gityusuke@gmail.com>
2015-10-27 00:44:57 +0900

    dbconnect.php

---
+++ dbconnect.php
@@ -0,0 +1,5 @@
+<?php
+    $db = mysqli_connect('mysql101.phy.lolipop.lan', 'LAA0656243', '33310ym', 'LAA0656243-twitterbbs')
+    or die(mysqli_connect_error());
+    mysqli_set_charset($db, 'utf8');
+?>
